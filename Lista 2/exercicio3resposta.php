d98bc4da4b2fe074063d4219436c117f507ec03e
waltneto <walter.gomes@fatec.sp.gov.br>
2025-02-27 21:20:12 -0300

    Lista 2 exercicio 3

---
+++ Lista 2/exercicio3resposta.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercicio 3 Resposta</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Resposta do Exercício 3</h1>
+  <?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+      $valor1 = $_POST['valor1'];
+      $valor2 = $_POST['valor2'];
+
+      $mult = $valor1 * $valor2;
+      echo "O valor da multiplicação é: $mult";
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
