7ab0e7537c90b9ce7fae164fe4904b4b0f2f33d7
waltneto <walter.gomes@fatec.sp.gov.br>
2025-02-27 21:19:45 -0300

    Lista 2 exercicio 2

---
+++ Lista 2/exercicio2.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercício 2</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Exercício 2</h1>
+
+  <form method="post" action="exercicio2resposta.php">
+    <div class="mb-3">
+      <label for="valor1" class="form-label">Informe o primeiro valor</label>
+      <input type="number" id="valor1" name="valor1" class="form-control" required="">
+    </div>
+
+    <div class="mb-3">
+      <label for="valor2" class="form-label">Informe o segundo valor</label>
+      <input type="number" id="valor2" name="valor2" class="form-control" required="">
+    </div>
+
+    <button type="submit" class="btn btn-primary">Enviar</button>
+  </form>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ Lista 2/exercicio2resposta.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Exercício 2 Resposta</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+  <h1>Resposta do Exercício 2</h1>
+  <?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    try {
+      $valor1 = $_POST['valor1'];
+      $valor2 = $_POST['valor2'];
+
+      $sub = $valor1 - $valor2;
+      echo "O valor da subtração é: $sub";
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+  }
+
+  ?>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
