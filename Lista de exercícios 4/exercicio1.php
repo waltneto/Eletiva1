6e13e29a2faf934e93e977278e24fafa4aeca3bd
waltneto <walter.gomes@fatec.sp.gov.br>
2025-03-13 20:39:37 -0300

    lista 4 1ª versão

---
+++ Lista de exercícios 4/exercicio1.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Exercicio1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php
+function contarCaracteres($palavra) {
+    return strlen($palavra); // Função interna do PHP para contar caracteres
+}
+
+// Entrada simulada (pode ser substituída por um formulário)
+$palavra = "exemplo";
+
+// Processamento e saída
+echo "A palavra '$palavra' possui " . contarCaracteres($palavra) . " caracteres.";
+?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
